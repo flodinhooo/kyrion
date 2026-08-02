29cfbc4c1007526409f6561a97a8f9dbb27761d8
FloCodin <rogenmoserflo@gmail.com>
2026-08-02 17:49:36 +0200

    Add: Spring Boot Flyway starter dependency to `core` service

--- services/core/build.gradle.kts
+++ services/core/build.gradle.kts
@@ -20,11 +20,11 @@ repositories {
 dependencies {
     implementation(platform("org.springframework.boot:spring-boot-dependencies:4.1.0"))
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-flyway")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-database-postgresql")
     implementation(kotlin("reflect"))
     runtimeOnly("org.postgresql:postgresql")
